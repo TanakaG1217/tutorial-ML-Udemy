2b47bf08e4c98cb7e21d76b7a486a9c1561865fe
TanakaG1217 <tanakagakuto1217@gmail.com>
2024-03-28 17:59:15 +0900

    main更新

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 import numpy as np 
 import pandas as pd 
+#import pandas_profiling #pdの拡張機能
+import matplotlib
+matplotlib.use('TkAgg') # GUIバックエンドを使用するためのもの
 import matplotlib.pyplot as plt
-import seaborn as sns
+import seaborn as sns #pltの拡張機能
+
 
 
 # データセット読み込み
@@ -11,6 +15,11 @@ DATA_GENDER_SUBMISSION = pd.read_csv('../dataset/gender_submission.csv')
 
 #出力行数設定
 pd.set_option('display.max_rows',900)
-print(pd.get_option('display.max_rows'))
+print(f"出力行数：{pd.get_option('display.max_rows')}")
 
-print (DATA_TRAIN)
+#trainデータ詳細
+print(f"\ntrainデータ詳細：{DATA_TRAIN.describe()}")
+
+#sns使用例
+sns.countplot(x='Pclass',hue='Survived',data=DATA_TRAIN)
+plt.show()  # この行を追加
